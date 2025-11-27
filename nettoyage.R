0c356cb315b14007b79e89a0e755561e42f6b429
Riwa Hachem Reda <riwahachemreda@gmail.com>
2025-11-27 16:24:54 +0100

    premier traitement + nettoyage

---
+++ nettoyage.R
@@ -0,0 +1,93 @@
+load("new_data")
+data_de_base = data_to_umontpellier
+rm(data_to_umontpellier)
+
+library(dplyr)
+library(lubridate)
+
+data_non_relancer <- data_de_base %>% 
+  filter(a_ete_relance == FALSE)
+
+data_non_relancer = subset(data_non_relancer, select = -a_ete_relance)
+
+data = data_non_relancer
+
+# Retirer status = en cours
+status_en_cours <- c(
+  "encours_azur",
+  "encours_commercial",
+  "encours_depot_banque",
+  "en_attente_azur",
+  "en_contestation_banque",
+  "attente_signature_client",
+  "envoi_bfc",
+  "potentiel",
+  "signature_offre_de_pret",
+  "accord_banque",
+  "fin_azur"
+)
+
+data <- data %>% 
+  filter(!status %in% status_en_cours)
+
+data <- data %>% 
+  filter(prochain_rdv <= as.Date("2025-11-01") | is.na(prochain_rdv))
+
+
+# Colonnes d'identifiants
+cols_id <- c(
+  "id_utilisateur", "id_rendez_vous", "premier_id_rendez_vous", "id_conso", 
+  "id_immo", "id_dossier","reference_pret_offre"
+)
+
+# Colonnes info personnelles 
+cols_perso <- c("profession_complement")
+
+# Colonnes datesuni
+cols_dates <- c(
+  "date_derniere_modification_dossier","date_premier_rdv_prevu", 
+  "date_creation_premier_rdv_prevu","date_heure_premier_rdv","date_heure_depot_banque", 
+  "date_heure_suivi_max", "date_rdv_en_cours_annulation", "date_heure_accord_banque",
+  "date_heure_contestation", "date_derniere_relation", "date_last_rdv_pas_fait",
+  "date_heure_min_depot_banque", "date_heure_min_refus_banque","coemprunteur_date_renonciation", 
+  "emprunteur_date_renonciation",  "date_rdv_en_cours_annulation", 
+  "consultation_doc_espace_client", "mise_a_dispo_espace_client", "date_suivi_max", "date_suivi_J1",
+  "date_validation_suivi_J1", "date_dossier_risque", "date_derniere_relation_banque", 
+  "date_last_rdv_realise", "date_derniere_relation_emise_client", "date_min_depot_banque",
+  "date_signature_op", "date_depot_banque"
+  )
+
+# Colonnes textuelles et autres 
+cols_autres <- c(
+  "libelle_support", "type_refus","mensualite_nouvelle", "montant_commission",
+  "risque_remarque", "risque_delai_remarque", "envoie_adp", "envoie_paa",
+  "epargne_refuse","honoraire", "garanties_assurance_coemprunteur", "garanties_assurance_emprunteur",
+  "attente_status", "montant_hib", "montant_hib_fictif", "montant_a_financer", "status_virtuel",
+  "pret_conso_montant_inital", "pret_conso_crd_all", "pret_conso_echeance_all", "pret_conso_echeance",
+  "pret_conso_crd", "pret_immo_montant_inital", "pret_immo_echeance_all", "pret_immo_echeance",
+  "pret_immo_crd_all", "pret_immo_crd", "etat_premier_rdv","categorie_relance", "date_heure_accord_banque",
+  "emprunteur_status_renonciation", "coemprunteur_status_renonciation", "emprunteur_quotite_renonciation",
+  "coemprunteur_quotite_renonciation", "last_etat_depot", "last_etat_depot_notaire", "etat_epargne",
+  "etat_rdv_signature_op", "etat_epargne_refus"
+)
+
+# Colonnes assurances
+cols_assurance <- c(
+  "assurance_groupe_coemprunteur", "assurance_groupe_emprunteur", "status_assurance_coemprunteur",
+  "status_assurance_emprunteur", "mandat_assurance_coemprunteur", "mandat_assurance_emprunteur",
+  "prime_assurance_coemprunteur", "prime_assurance_emprunteur", "quotite_assurance_coemprunteur",
+  "quotite_assurance_emprunteur", "compagnie_assurance_coemprunteur", "compagnie_assurance_emprunteur"
+)
+
+# Colonnes à supprimer 
+cols_a_supprimer <- c(
+  cols_id,
+  cols_perso,
+  cols_dates,
+  cols_autres,
+  cols_assurance
+)
+
+data <- data %>% select(-all_of(cols_a_supprimer))
+
+save(data, file = "data.RData")
---
+++ traitement.R
@@ -0,0 +1,86 @@
+library(dplyr)
+library(lubridate)
+
+load("data.RData")
+
+data$montant[data$montant < 12000] <- "inconnu"
+
+data$taux_offre[data$taux_offre > 300] <- "inconnu"
+
+data$date_realisation_premier_rdv <- ifelse(is.na(data$date_realisation_premier_rdv), 0, 1)
+data$premier_rdv_realise <- data$date_realisation_premier_rdv
+data$date_realisation_premier_rdv <- NULL
+
+data$signature_electronique <- ifelse(
+  data$signature_electronique == "Y", 1,
+  ifelse(data$signature_electronique == "N", 0, "inconnu"))
+
+data <- data %>% 
+  mutate(
+    age_emprunteur = as.numeric(floor((date_creation - emprunteur_date_naissance) / 365.25)),
+    age_coemprunteur = as.numeric(floor((date_creation - coemprunteur_date_naissance) / 365.25))
+  ) %>%
+  mutate(
+    age_emprunteur = ifelse(age_emprunteur < 18 | age_emprunteur > 90, "inconnu", age_emprunteur),
+    age_coemprunteur = ifelse(age_coemprunteur < 18 | age_coemprunteur > 90, "inconnu", age_coemprunteur)
+  )%>%
+  select(-date_creation, -emprunteur_date_naissance, -coemprunteur_date_naissance)
+
+data$est_refuse <- ifelse(is.na(data$date_refus), 0, 1)
+data$date_refus <- NULL
+
+data$rdv_notaire <- ifelse(is.na(data$date_rdv_notaire), 0, 1)
+data$date_rdv_notaire <- NULL
+
+data$banque_accord <- ifelse(is.na(data$date_premier_accord), 0, 1)
+data$date_premier_accord <- NULL
+
+data <- data %>%
+  mutate(
+    est_conteste = case_when(
+      # 1 : deux dates présentes ET contestation après refus
+      !is.na(date_min_refus_banque) & !is.na(date_contestation) &
+        date_contestation > date_min_refus_banque ~ "1",
+      
+      # 0 : refus présent mais pas de contestation
+      !is.na(date_min_refus_banque) & is.na(date_contestation) ~ "0",
+      
+      # "inconnu" : aucune des deux dates
+      is.na(date_min_refus_banque) & is.na(date_contestation) ~ "inconnu",
+      
+      # NA : contestation présente mais pas de refus
+      is.na(date_min_refus_banque) & !is.na(date_contestation) ~ NA_character_
+    )
+  )
+data <- data %>% select(-date_min_refus_banque, -date_contestation)
+
+data$est_conteste <- factor(data$est_conteste, levels = c("0","1","inconnu"))
+
+data$est_encaisse <- ifelse(is.na(data$date_encaissement_commercial), 0, 1)
+data$date_encaissement_commercial <- NULL
+
+data <- data %>%
+  mutate(
+    retour_bfc = case_when(
+      # 1 : envoi + réception
+      (!is.na(date_envoi_bfc) & !is.na(date_reception_bfc)) | (!is.na(date_envoi_bfc) & !is.na(date_potentiel)) ~ "1",
+      
+      # 0 : envoi mais pas de réception
+      !is.na(date_envoi_bfc) &  is.na(date_reception_bfc) & is.na(date_potentiel) ~ "0",
+      
+      # "inconnu" : rien envoyé, rien reçu
+      is.na(date_envoi_bfc)  &  is.na(date_reception_bfc) & is.na(date_potentiel) ~ "inconnu",
+      
+      # NA : retour mais pas d'envoi
+      is.na(date_envoi_bfc)  & (is.na(date_reception_bfc) | is.na(date_potentiel)) ~ NA_character_
+    )
+  )
+data <- data %>% select(-date_envoi_bfc, -date_reception_bfc, -date_potentiel)
+data$retour_bfc <- factor(data$retour_bfc, levels = c("0","1","inconnu"))
+
+data$est_frigo <- ifelse(is.na(data$date_fin_previsible_frigo), 0, 1)
+data$date_fin_previsible_frigo <- NULL
+
+data$apl[data$apl < 5] <- "inconnu"
+
+data$allocation_familiale[data$allocation_familiale < 10] <- "inconnu"
