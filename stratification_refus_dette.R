a6ef4ea12d5757d7893ce1fe98aa9fc77f25ae14
coralieromani <coralie.romani-de-vinci@etu.umontpellier.fr>
2025-12-01 10:26:45 +0100

    correction

--- stratification_refus_dette.R
+++ stratification_refus_dette.R
@@ -4,10 +4,9 @@ load("data_nouveau.RData")
 data_accord = data[data$status == "encaisse",]
 donnees_manquantes_a <- colSums(is.na(data_accord)) / nrow(data_accord) * 100
 lapply(data_accord, unique)
-data_accord = subset(data_accord, select=-c(status,motif_refus,type_support,premier_rdv_realise,est_refuse,banque_accord,est_encaisse))
+data_accord = subset(data_accord, select=-c(status,motif_refus,premier_rdv_realise,est_refuse,banque_accord,est_encaisse))
 # status : encaisse
 # motif_refus : NA
-# type_support = Internet
 # dette_retard_loyer : que 1 : 1996 et que des : 0 ou NA
 # premier_rdv_realise : 1
 # est_refuse : 0
@@ -17,9 +16,8 @@ data_accord = subset(data_accord, select=-c(status,motif_refus,type_support,prem
 data_frigo = data[data$status == "frigo",]
 donnees_manquantes_f <- colSums(is.na(data_frigo)) / nrow(data_frigo) * 100
 lapply(data_frigo, unique)
-data_frigo = subset(data_frigo, select=-c(status,type_support,reserve_levee,dette_retard_loyer,etude_partagee,premier_rdv_realise,rdv_notaire,est_encaisse))
+data_frigo = subset(data_frigo, select=-c(status,reserve_levee,dette_retard_loyer,etude_partagee,premier_rdv_realise,rdv_notaire,est_encaisse))
 # status : frigo
-# type_support : Internet
 # motif_refus : Refus CF après 1e rendez vous ou NA
 # scoring : FRIGO ou NA
 # reserve_levee : N
@@ -35,8 +33,7 @@ data_frigo = subset(data_frigo, select=-c(status,type_support,reserve_levee,dett
 data_refus <- data %>% filter(!status %in% c("encaisse", "frigo"))
 donnees_manquantes_r <- colSums(is.na(data_refus)) / nrow(data_refus) * 100
 lapply(data_refus, unique)
-data_refus = subset(data_refus, select=-c(type_support,est_encaisse))
-# type_support : Internet
+data_refus = subset(data_refus, select=-c(est_encaisse))
 # est_encaisse : 0
 
 data_refus_dette <- data_refus[!is.na(data_refus$dette_famille_ami), ]
@@ -45,7 +42,7 @@ lapply(data_refus_dette, unique)
 
 data_refus_sans_dette <- data_refus[is.na(data_refus$dette_famille_ami), ]
 donnees_manquantes_r2 <- colSums(is.na(data_refus_sans_dette)) / nrow(data_refus_sans_dette) * 100
-lapply(data_refus_sans_dette[,1:56], unique)
+lapply(data_refus_sans_dette, unique)
 data_refus_sans_dette = subset(data_refus_sans_dette, select=-c(dette_retard_loyer,dette_famille_ami,
                                                                 dette_retard_impot,dette_decouvert,
                                                                 dette_autre, dette_saisie_sur_salire,
@@ -68,5 +65,3 @@ data_refus_sans_dette = subset(data_refus_sans_dette, select=-c(dette_retard_loy
 # DONNÉES MANQUANTES TABLE REFUS AVEC DETTES
 
 # DONNÉES MANQUANTES TABLE REFUS SANS DETTES
-
-
