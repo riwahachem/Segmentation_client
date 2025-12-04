d3b42f9f577f8793127f33fac000dabff6d2f0f5
Wahelelm <welmazzouji72@gmail.com>
2025-12-04 10:59:02 +0100

    modif strat

--- Data_cleaning/analyse_faisabilite_strat.R
+++ Data_cleaning/analyse_faisabilite_strat.R
@@ -2,37 +2,36 @@ library(dplyr)
 load("Data_Cleaning/data_nouveau.RData")
 
 data_accord <- data[data$status == "encaisse",]
-lapply(data_accord, function(x) head(unique(x,7)))
 data_accord <- data_accord %>%
-  select(-c(status, est_refuse, banque_accord, est_encaisse, est_frigo, motif_refus))
-# retour_bfc 
+  select(-c(status, est_refuse, banque_accord, est_encaisse, est_frigo, motif_refus, premier_rdv_realise))
+data_accord <- data_accord %>%
+  mutate(retour_bfc = as.factor(ifelse(is.na(retour_bfc), "Non Defini", as.character(retour_bfc))))
+lapply(data_accord, function(x) head(unique(x,7)))
+data_accord <- data_accord %>% distinct()
+data_accord = subset(data_accord, select=-c(cle_unique_tel,cle_unique_mail))
+
 data_frigo <- data[data$status == "frigo",]
+data_frigo <- data_frigo %>% select(-c(status,reserve_levee,etude_partagee,rdv_notaire,est_encaisse,est_frigo, banque_accord, est_refuse,motif_refus,premier_rdv_realise))
+
 lapply(data_frigo, function(x) head(unique(x,7)))
-data_frigo <- data_frigo %>% select(-c(status,reserve_levee,etude_partagee,rdv_notaire,est_encaisse))
-data_frigo <- data_frigo %>%
-  mutate(
-    signature_electronique = as.factor(ifelse(is.na(signature_electronique), 0, 1)),
-    
-    scoring = as.factor(ifelse(is.na(scoring), "Non Defini", as.character(scoring))),
-    
-    motif_refus = as.factor(ifelse(is.na(motif_refus), "Pas de motif", as.character(motif_refus))),
-    
-    risque_delai = as.factor(ifelse(is.na(risque_delai), 0, as.character(risque_delai)))
-  )
-# Problème : est_frigo : 0/1, banque_accord 0/1, est_refuse 0/1
+data_frigo <- data_frigo %>% distinct()
+data_frigo = subset(data_frigo, select=-c(cle_unique_tel,cle_unique_mail))
+data_frigo$risque_delai <- as.factor(ifelse(is.na(data_frigo$risque_delai), 0, 1))
+data_frigo$scoring <- as.factor(ifelse(is.na(data_frigo$scoring), 0, 1))
+
 data_refus_base <- data %>% filter(!status %in% c("encaisse", "frigo"))
 
 data_refus_prop <- data_refus_base %>% filter(type_dossier == "proprietaire")
 data_refus_prop <- data_refus_prop %>% distinct()
 data_refus_prop$type_dossier <- NULL
-lapply(data_refus_prop, function(x) head(unique(x,7)))
 data_refus_prop <- data_refus_prop %>% select(-c(est_encaisse,hebergement_gratuit))
-# dans hebergement gratuit y a que des NA 
+lapply(data_refus_prop, function(x) head(unique(x,7)))
+data_refus_prop <- data_refus_prop %>% distinct()
+data_refus_prop = subset(data_refus_prop, select=-c(cle_unique_tel,cle_unique_mail))
 
 data_refus_loc <- data_refus_base %>% filter(type_dossier != "proprietaire")
 data_refus_loc <- data_refus_loc %>% distinct()
 data_refus_loc$type_dossier <- NULL
-lapply(data_refus_loc, function(x) head(unique(x,7)))
 data_refus_loc <- data_refus_loc %>% select(-c(rdv_notaire,est_encaisse))
 data_refus_loc$type_garantie <- as.factor(ifelse(is.na(data_refus_loc$type_garantie), 1, 0))
 data_refus_loc$part_immo <- as.factor(ifelse(is.na(data_refus_loc$part_immo), 1, 0))
@@ -40,21 +39,27 @@ data_refus_loc$reserve_levee <- as.factor(ifelse(is.na(data_refus_loc$reserve_le
 data_refus_loc$etude_partagee <- as.factor(ifelse(is.na(data_refus_loc$etude_partagee), 1, 0))
 data_refus_loc$risque_delai <- as.factor(ifelse(is.na(data_refus_loc$risque_delai), 1, 0))
 data_refus_loc$signature_electronique <- as.factor(ifelse(is.na(data_refus_loc$signature_electronique), 1, 0))
+lapply(data_refus_loc, function(x) head(unique(x,7)))
+data_refus_loc <- data_refus_loc %>% distinct()
+data_refus_loc = subset(data_refus_loc, select=-c(cle_unique_tel,cle_unique_mail))
 
 cat("Taux de valeurs manquantes par sous groupe")
 
 cat("accord")
-print(colSums(is.na(data_accord)) / nrow(data_accord) * 100)
+a <- print(colSums(is.na(data_accord)) / nrow(data_accord) * 100)
+sort(a)
 
 cat("frigo")
-print(colSums(is.na(data_frigo)) / nrow(data_frigo) * 100)
+b <- print(colSums(is.na(data_frigo)) / nrow(data_frigo) * 100)
+sort(b)
 
 cat("refus proprietaire")
-print(colSums(is.na(data_refus_prop)) / nrow(data_refus_prop) * 100)
+c <- print(colSums(is.na(data_refus_prop)) / nrow(data_refus_prop) * 100)
+sort(c)
 
 cat("refus locataire")
-print(colSums(is.na(data_refus_loc)) / nrow(data_refus_loc) * 100)
-
+d <- print(colSums(is.na(data_refus_loc)) / nrow(data_refus_loc) * 100)
+sort(d)
 
 save(data_accord, file = "Data_Cleaning/data_accord.RData")
 save(data_frigo, file = "Data_Cleaning/data_frigo.RData")
